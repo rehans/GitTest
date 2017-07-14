b19f3bf39cdd714ca93de09cda0fd4ad91d71754
René Hansen <rene@hansen-audio.de>
2017-07-14 11:13:01 +0200

    Quick and dirty test app for Jack2 for Windows x64

---
+++ FindJack.cmake
@@ -0,0 +1,36 @@
+# SDKInclusion : Vst3Sdk
+# SDKInclusion : Vst3Sdk
+
+# - Try to find Jack Audio Libraries
+# libjack; libjackserver;
+# Once done this will define
+#  LIBJACK_FOUND - System has libjack.so
+#  LIBJACK_INCLUDE_DIRS - The jack.h include directories
+#  LIBJACK_LIBRARIES - The libraries needed to use jack
+
+# Install jack on Ubuntu: apt-get install libjack-jackd2-dev
+# Add user to audio group: usermod -a -G audio <yourUserName>
+
+find_path(LIBJACK_INCLUDE_DIR
+	NAMES
+		jack/jack.h
+	PATHS
+		$ENV{PROGRAMFILES}/Jack/includes
+)
+
+find_library(LIBJACK_libjack_LIBRARY
+	NAMES
+		libjack64 jack64
+	PATHS
+		$ENV{PROGRAMFILES}/Jack/lib
+)
+
+set(LIBJACK_LIBRARIES ${LIBJACK_libjack_LIBRARY})
+set(LIBJACK_LIBRARY ${LIBJACK_LIBRARIES})
+set(LIBJACK_INCLUDE_DIRS ${LIBJACK_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LIBJACK_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(LIBJACK  DEFAULT_MSG
+                                  LIBJACK_LIBRARY LIBJACK_INCLUDE_DIR)
---
+++ README.md
@@ -0,0 +1,10 @@
+#GitTest
+##How to
+```
+git clone https://github.com/rehans/GitTest
+mkdir GitTestBuild
+cd GitTestBuild
+cmake -G"Visual Studio 14 2015 Win64"
+```
+
+Open .sln file and build.
